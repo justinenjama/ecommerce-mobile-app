29c48f9d5db00e6e247a5e70d28542ea40900a41
justinenjama <justinenjama33@gmail.com>
2026-02-24 18:04:40 +0300

    db config & tools setu added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import path from 'path';
 import { ENV } from './config/env.js';
+import { connectDB } from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 const __dirname = path.resolve();
+
+app.use(clerkMiddleware({    // adds auth Object under the req => re.auth
+    secretKey: ENV.CLERK_SECRET_KEY,
+    publishableKey: ENV.CLERK_PUBLISHABLE_KEY,
+}));
 
 // make your app ready for deployment
 if (ENV.NODE_ENV === 'production') {
@@ -15,9 +22,9 @@ if (ENV.NODE_ENV === 'production') {
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
+  connectDB();
 });
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ status: "ok" });
 });
-
